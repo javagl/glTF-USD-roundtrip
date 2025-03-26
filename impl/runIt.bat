98e66c34e6967355c65777c977a90e4a9619783c
Marco Hutter <javagl@javagl.de>
2025-03-26 15:40:51 +0100

    Update and comment batch file

--- impl/runIt.bat
+++ impl/runIt.bat
@@ -1,1 +1,4 @@
-blender -P blender_conversions.py -P blender_utilities.py -P io_utilities.py -P roundtrip.py -P roundtrip_runner.py -P roundtrip_utilities.py -- %1 %2 %3 %4 %5 %6 %7 %8 %9
+REM Run Blender, passing in the all ".py" files that are required
+REM (optionally passing along additional arguments)
+
+blender.exe -P blender_conversions.py -P blender_utilities.py -P io_utilities.py -P roundtrip.py -P roundtrip_runner.py -P roundtrip_utilities.py -- %1 %2 %3 %4 %5 %6 %7 %8 %9
